daa43dce3691a52ae8a575118e4684b174e385f0
OliviaVitali <47398506+OliviaVitali@users.noreply.github.com>
2019-10-14 15:13:25 -0400

    update for stats for characters

--- Stats.java
+++ Stats.java
@@ -1,4 +1,44 @@
 public class Stats {
 
-    //todo Cam, this is on you buddy
+    private int hp, strength, speed, defense;
+    //creates default stats
+    public Stats(){
+        hp=1;
+        strength=1;
+        speed=1;
+        defense =1;
+    }
+    //creates states based on input
+    public Stats(int hp, int strength, int speed, int def){
+        this.hp = hp;
+        this.strength = strength;
+        this.speed = speed;
+        this.defense = def;
+    }
+    
+    public void damageCalculations(int hit){
+    hp -= hit-defense;
+    }
+    
+    public void changeStr(int i){
+        strength += i;
+    }
+    public void changeDef(int i){
+        defense += i;
+    }
+    public void changeSpd(int i){
+        speed+= i;
+    }
+    public int getHP(){
+        return hp;
+    }
+    public int getStr(){
+        return strength;
+    }
+    public int getDef(){
+        return defense;
+    }
+    public int getSpd(){
+        return speed;
+    }
 }
