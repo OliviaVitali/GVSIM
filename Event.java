dbae37c4678a8bcb5bd01f71c2e997c1cbe5b94c
OliviaVitali <47398506+OliviaVitali@users.noreply.github.com>
2019-11-22 13:12:38 -0500

    Comments for Event
    Added comments to clarity event

--- Event.java
+++ Event.java
@@ -4,13 +4,21 @@
  * bridge
  */
 public class Event {
-
+    /** name of the event*/
     String name;
+    /** flavortext that should be printed to the GUI*/
     protected String flavorText;
+    /** associated inventory item*/
     protected InventoryItem inventoryItem;
+    /** related npc if needed for the event*/
     protected Character npc;
+    /** if the event changes the location, the String is the new location */
     protected String location;
 
+    /**
+    * Default constructor for events
+    * generally should be avoided being used
+    */
     public Event(){
         name = "Event";
         flavorText = "flavorText ";
@@ -19,6 +27,11 @@ public class Event {
         location = null;
     }
 
+    /**
+    * Constructor that initializes name and flavorText
+    * @param name is the name of the event
+    * @param flavorText is the text that should be printed on the GUI
+    */
     public Event(String name, String flavorText){
         this.name = name;
         this.flavorText = flavorText;
@@ -27,53 +40,94 @@ public class Event {
         location = null;
     }
 
+    /**
+    * returns the new location name (NOT a location class object) if 
+    * location will be changed
+    * @return the new location name
+    */
     public String getLocation() {
         return location;
     }
 
+    /**
+    * changes location in game (may not be used)
+    */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+    * getter for the name of the book
+    * @returns name of the event
+    */
     public String getName() {
         if (name != null) return name;
         return null;
     }
 
+    /**
+    * setter for the name of the event
+    */
     public void setName(String name) {
         if (name != null) this.name = name;
     }
 
+    /**
+    * getter for flavor text
+    * @return string with flavorText to be print on GUI
+    */
     public String getFlavorText() {
         if (flavorText != null)return flavorText;
         return null;
     }
 
+    /**
+    * Setter for flavor text.  Appends to current text (not replaces it)
+    */
     public void setFlavorText(String flavorText) {
         if (flavorText != null) this.flavorText += flavorText;
     }
 
+    /**
+    * @returns inventory item if associated with the event
+    */
     public InventoryItem getInventoryItem() {
         if (inventoryItem != null) return inventoryItem;
         return null;
     }
 
+    /**
+    * setter for inventory item
+    */
     public void setInventoryItem(InventoryItem inventoryItem) {
         if (inventoryItem != null) this.inventoryItem = inventoryItem;
     }
 
+    /**
+    * getter for NPC
+    * @returns character NPC associated with event
+    */
     public Character getNpc() {
         if (npc != null)return npc;
         return null;
     }
 
-
+    /**
+    * setter for NPC associated with event
+    * could be player but that goes against the game's design
+    */
     public void setNpc(Character npc) {
+        //cannot be null
         if (npc != null) {
             this.npc = npc;
         }
     }
 
+    /**
+    * gets the speech associated with the NPC
+    * may not be used
+    * @ returns string with npc's response
+    */
     public String getSpeech() {
         if (npc != null) {
             return npc.getSpeech(name);
