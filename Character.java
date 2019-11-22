77ce273163db0850ba3d1d5e071f1d8a236c4732
OliviaVitali <47398506+OliviaVitali@users.noreply.github.com>
2019-11-22 12:46:26 -0500

    Comment update
    Added Comments to Character

--- Character.java
+++ Character.java
@@ -2,58 +2,109 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Character {
-
+    /** name of character*/
     private String charName;
+    /** User command then speech options for character */
     private Map<String, String> speechOptions;
+    /** list of inventory items the character has */
     private InventoryItem[] inventoryItems;
+    /** stats used for fighting*/
     private Stats charStats;
+    /**determines if this is an NPC or the player*/
     private boolean isPlayer;
+    /** flavor text to describe the character */
     private String description;
     private String list;
 
 
-
+/***********************************************************
+*Getter for Character name
+* @return charName which is the character's name
+***************************************************/
     public String getCharName() {
         return charName;
     }
 
+    /**
+    *setter for character name
+    */
     public void setCharName(String charName) {
         this.charName = charName;
     }
 
+    /**
+    * gets all conversation options for te character
+    * @return list which is a list of all speech options
+    */
     public String getSpeechOptions() {
         return list;
     }
 
+    /**
+    * setter to add speach options
+    * @param call is the user command
+    * @param response is the NPC's response
+    */
     public void setSpeechOptions(String call, String response) {
         this.speechOptions.put(call, response);
         list += "\n " + call.toLowerCase();
     }
 
+    /**
+    * return intentory item in slot i
+    * i is the index of the inventory item
+    * @return the inventory item being requested at index i
+    */
     public InventoryItem getInventoryItems(int i) {
         return inventoryItems[i];
     }
-
+    /**
+    * setter for inventory item
+    * @param inventoryItems is the new inventory item
+    * @param i is the index intended in the list
+    */
     public void setInventoryItem(InventoryItem inventoryItems, int i) {
         this.inventoryItems[i] = inventoryItems;
     }
 
+    /**
+    * getter used in fighting
+    * @return character's stats
+    */
     public Stats getCharStats() {
         return charStats;
     }
 
+    /**
+    * Sets Character's stats to the input
+    * @param charStats is the new character status numbers
+    */
     public void setCharStats(Stats charStats) {
         this.charStats = charStats;
     }
 
+    /**
+    * checks if this is the player
+    * @returns isPlayer is true when the object is the player
+    */
     public boolean isPlayer() {
         return isPlayer;
     }
 
+    /**
+    * setter to change if object is the player or not
+    * @param player is the true false value for if they are the player
+    */
     public void setIsPlayer(boolean player) {
         isPlayer = player;
     }
 
+    
+    /**
+    * Default constructor for a character
+    * if not changed, the character is Jenny.  Note: Jenny should not be
+    * in the game so if you find her, something has gone wrong or is incomplete
+    */
     public Character() {
         charName = "Jenny";
         description = "\nA girl with a cell phone in her hand.";
@@ -66,13 +117,25 @@ public class Character {
         isPlayer = false;
     }
 
+    /**
+    * gets the response associated with the user's input
+    * @return Character's response to the user input
+    */
     public String getSpeech(String userCommand) {
         return speechOptions.get(userCommand);
     }
 
+    /**
+    * checks if the player can fight the character
+    */
     public boolean isFightable() {
+        //you cannot fight yourself
+        if (isPlayer) return false;
+        //can't fight a player who has no Stats
         if (charStats == null)
             return false;
+        //you cannot fight dead characters
+        if (charStats.getHP() <0) return false;
         return true;
     }
 }
