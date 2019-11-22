1d427b15740f4ee5a627886c09dbc81d641f371c
OliviaVitali <47398506+OliviaVitali@users.noreply.github.com>
2019-11-22 13:14:56 -0500

    GUI comments
    Added comments to GUI class

--- GUI.java
+++ GUI.java
@@ -3,13 +3,19 @@ import java.awt.*;
 
 public class GUI {
 
+    /**
+    * main function for game
+    * creates GUI for the game
+    */
     public static void main(String[] args){
-
+        /** frame that hold the game */
         JFrame frame = new JFrame("GV Simulator");
+        //sets defaults for frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setBackground(Color.PINK);
 
+        /** panel is the game itself */
         GamePanel panel = new GamePanel();
 
         //Adding Components to the frame.
